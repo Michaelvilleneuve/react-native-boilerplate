da451e25092ac3c6296cfc8485739ba3e6391831
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-01-17 14:52:16 +0100

    Improved boilerplate with initial redux setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
   render() {
     return (
-      <View>
-      <Text>Boilerplate</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Boilerplate</Text>
+        </View>
+      </Provider>
     );
   }
 }
